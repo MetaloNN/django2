61c5ce391af8621c074a77351a01303934fad967
MetaloNN <herrera.a.nicolas@gmail.com>
2022-09-12 18:46:05 -0300

    Prueba django completa sin terminar

---
+++ Home/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'home.html')
+
+def cafe(request):
+
+    cafe = [{'name': 'Capuccino', 'img': 'capuccino.jpg', 'valor': 3000 ,'descripcion': 'El capuccino es cafe compuesto por expreso de café y leche con espuma de leche'},
+                {'name': 'Café Americano', 'img': 'cafe.webp', 'valor': 2500, 'descripcion': 'El Café americano consta de cafe más agua'},
+                {'name': 'Latte', 'img': 'latte.jpg', 'valor': 2000, 'descripcion': 'El late es un hermano del capuccino pero mas vistoso, por su corte entre el cafe y la leche'},
+                {'name': 'Moca', 'img': 'moca.jpg', 'valor': 3500, 'descripcion': 'El café moca es un capuccino con chocolate'},
+                {'name': 'Expreso', 'img': 'expreso.webp', 'valor': 1500, 'descripcion': 'El Expreso es un corto de café concentrado'},
+             ]
+
+    return render(request, 'cafe.html', {
+        'cafe': cafe
+    })
+
+def pasteles(request):
+
+    pasteles = [{'nombre': 'Tres leches', 'img':'tres.jpg', 'valor': 3000, 'descripcion':'Trozo de torta tres leches'},
+                {'nombre': 'Mil hojas', 'img':'mil.jpg', 'valor': 3000, 'descripcion':'Trozo de torta mil hojas'},
+                ]
+    return render(request, 'pasteles.html', {
+        'pasteles': pasteles
+    })
+
+def artesanales(request):
+    return render(request, 'artesanales.html')
+
+def marcas(request):
+    return render(request, 'marcas.html')
